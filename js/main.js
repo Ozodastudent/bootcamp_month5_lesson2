bd0c03e5e088ca235b5d4a6358e42ea2eed7c385
Ozodastudent <shakhobiddinovaozoda@yandex.com>
2022-10-07 17:23:52 +0500

    Last update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 var formEl = document.querySelector(".form");
 var inputNum = document.querySelector(".input_number");
 var outNum = document.querySelector(".output_number");
-
 formEl.addEventListener("submit", function (evt) {
   evt.preventDefault();
   var inputValue = inputNum.value;
-  if (inputValue % 3 == 0 && inputValue % 5 == 0)
-    outNum.textContent = "FizzBuzz😊";
-  else if (inputValue % 3 == 0) outNum.textContent = "Fizz🙂";
-  else if (inputValue % 5 == 0) outNum.textContent = "Buzz🤩";
-  else outNum.textContent = "😒Nothing";
+  var newStr = "";
+  if (inputValue % 3 == 0) {
+    newStr += "Fizz😊";
+  }
+  if (inputValue % 5 == 0) {
+    newStr += "Buzz😎";
+  }
+  outNum.textContent = newStr || `Nothing`;
 });
